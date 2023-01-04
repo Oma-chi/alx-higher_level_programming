486395a6305c72df03cb3b5e5c3f917f4f808586
Oma-chi <jacintaokeke97@gmail.com>
2023-01-03 21:05:55 -0800

    test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#/usr/bin/python3
+"""
+Describes a function that adds two integers
+"""
+
+def add_integer(a, b=98):
+    """Take the arguments to add.
+    a: One of the numbers, default not defined.
+    b: The other number, default 98.
+    Return: int(a + b)
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
