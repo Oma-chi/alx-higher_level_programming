476984db1c860005fa6f94274c78a44825c27514
Okeke Jacinta Chioma <jacintaokeke97@gmail.com>
2023-01-10 20:44:23 -0800

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 
 def read_file(filename=""):
     """prints the content of a UFT-8 text file"""
-    with open(filename, encoding="UTF-8") as f
+    with open(filename, encoding="utf-8") as f
     print(f.read(), end="")
