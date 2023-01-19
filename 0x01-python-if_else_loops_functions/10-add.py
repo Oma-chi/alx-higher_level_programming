9401606dc6b6df2c63f8519b24fe8d6cdc8004ea
Okeke Jacinta Chioma <jacintaokeke97@gmail.com>
2023-01-19 02:10:19 -0800

    if else loops functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def add(a, b):
+    """function that adds two numbers a and b"""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """a function that returns the a to the poweer of b"""
+    return (a ** b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
